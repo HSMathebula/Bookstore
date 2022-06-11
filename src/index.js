2065628c033ccc6f0da0abc3d8e1d8e11a31ec84
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-11 09:24:41 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 import BookstoreContainer from './pages/AppContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Router>
+    <Router>
       <BookstoreContainer />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 reportWebVitals();
