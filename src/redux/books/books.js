683a80c965c3448bcf20edc0072c3cba5b656095
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-16 15:49:24 +0200

    Add actions and reducer for add-book and delete book ducks pattern

---
+++ src/redux/books/books.js
@@ -0,0 +1,33 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const DELETE_BOOK = 'bookstore/books/DELETE_BOOK';
+
+const initialState = {
+  books: [],
+};
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const deleteBook = (id) => ({
+  type: DELETE_BOOK,
+  id,
+});
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return {
+        ...state,
+        books: [...state.books, action.book],
+      };
+    case DELETE_BOOK:
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.book.id),
+      };
+    default:
+      return state;
+  }
+};
