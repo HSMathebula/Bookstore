0f772887f79475cb5c40c62700cf724a08c3cd4b
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-16 20:51:17 +0200

    Cooneting redux with react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
@@ -9,10 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router> */}
     <Provider store={store}>
       <App />
     </Provider>
-    {/* </Router> */}
   </React.StrictMode>,
 );
