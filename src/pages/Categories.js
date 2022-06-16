e97b17aadc64a3367fc12e87a31e7de588b77e9d
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-16 15:39:05 +0200

    Install redux and setup structure

--- src/pages/Categories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Categorypage = () => (
-  <div className="quoteText">
-    <button type="button">Check status</button>
-  </div>
-);
-
-export default Categorypage;
