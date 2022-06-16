bd3e881861fd45585e8aa58a4f8a43e6db6c0636
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-16 17:11:21 +0200

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
+import { Link, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
-import NavbarComponent from './components/Navbar';
-import FormComponent from './components/Form';
-import './App.css';
 
 const App = () => (
-  <>
-    <Router>
-      <NavbarComponent />
-      <div className="app">
-        <div className="app-container">
-          <Switch>
-            <Route path="/categories">
-              <Categories />
-            </Route>
-            <Route exact path="/">
-              <Books />
-              <FormComponent />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  </>
+  <div>
+    <header>
+      <h1>BookStore CMS</h1>
+      <nav>
+        <Link style={{ marginRight: '1rem' }} to="/">
+          Books
+        </Link>
+        <Link to="/categories">CATEGORIES</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </div>
 );
 
 export default App;
