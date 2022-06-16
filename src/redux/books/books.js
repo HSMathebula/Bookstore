4d114ae1d928b50fc347f69339d146e603031ae0
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-16 23:40:18 +0200

    Fetching data from pai

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,52 @@
-const ADDBOOk = 'book-store/books/ADD BOOK';
+const ADDBOOK = 'book-store/books/ADD BOOK';
 const REMOVEBOOK = 'book-store/books/REMOVE BOOK';
+const SETLOADING = 'book-store/books/SET LOADING';
+const GETBOOKS = 'book-store/books/GET BOOK';
 
-const defaultState = [
-  { genre: 'Comedy', title: 'Harry Potter', author: 'JK Rowling' },
-  { genre: 'Sport', title: 'I am footbAll', author: 'Zlatan Ibrahimovic' },
-];
+const defaultState = {
+  loading: false,
+  books: [],
+};
 
 export default function booksReducer(state = defaultState, action) {
   switch (action.type) {
-    case ADDBOOk:
-      return [...state, action.book];
+    case ADDBOOK:
+      return { ...state, books: [...state.books, action.book] };
     case REMOVEBOOK:
-      return [
-        ...state.slice(0, action.index),
-        ...state.slice(action.index + 1),
-      ];
-    default:
+      return {
+        ...state,
+        books: state.books.filter((el) => el.item_id !== action.itemid),
+      };
+      case SETLOADING:
+      return { ...state, loading: action.loading };
+      case GETBOOKS:
+        return { ...state, books: [...action.books] };
+      default:
       return state;
   }
 }
 
 export function addBook(book) {
-  return { type: ADDBOOk, book };
+  return { type: ADDBOOK, book };
 }
 
 export function removeBook(index) {
   return { type: REMOVEBOOK, index };
+}
+
+export function getBooks() {
+  return async (dispatch) => {
+    dispatch(setLoading(true));
+    const response = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/38b59qFydtEoTzEgXCix/books')
+      .then((response) => response.text())
+      .then((result) => result)
+      .catch((error) => `Loading Failed. ${error}`);
+    const books = [];
+    const tempArr = JSON.parse(response);
+    Object.keys(tempArr).forEach((e) => {
+      books.push({ ...tempArr[e][0], item_id: e });
+    });
+    dispatch(setLoading(false));
+    dispatch({ type: GETBOOKS, books });
+  };
 }
